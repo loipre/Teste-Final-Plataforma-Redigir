9e6df007b4050dd52d7682b8d6ae553cd2179640
Felipe da Silva Freitas <ffellipesfreitas@gmail.com>
2021-10-07 11:42:10 -0300

    Initial commit

---
+++ PermissionManagement.MVC/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PermissionManagement.MVC.Controllers
+{
+    [Authorize(Roles = "Basic , Admin , SuperAdmin")]
+    public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
